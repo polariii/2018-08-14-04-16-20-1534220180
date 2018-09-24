748ce6bcb1f3ca6336cb2928ac93166a2719fa3c
polariii <polaryw@gmail.com>
2018-09-24 14:19:23 +0800

    stats

--- main/main.js
+++ main/main.js
@@ -1,18 +1,33 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.count()}
+o) 平均值 = ${sequence.avg()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.input = input;
   }
 
   minimum() {
-    // Write your code here
+    return Math.min.apply(null, this.input);
   }
 
-  // Write your code here
+  maximum() {
+    return Math.max.apply(null, this.input);
+  }
+
+  count() {
+    return this.input.length;
+  }
+
+  avg() {
+    var sum = 0;
+    for (var i =0; i < this.input.length; i++){
+      sum += this.input[i];
+    };
+    return ~~(sum/this.input.length*100)/100;
+  }
 }
